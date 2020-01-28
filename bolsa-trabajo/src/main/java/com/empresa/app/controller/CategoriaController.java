e1dd662cb56b02ec1bf6ab5ebc3b1451e96807c9
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-27 22:01:17 -0600

    List of categorias, added

--- bolsa-trabajo/src/main/java/com/empresa/app/controller/CategoriaController.java
+++ bolsa-trabajo/src/main/java/com/empresa/app/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.empresa.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +22,12 @@ public class CategoriaController
 	
 	
 	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String listaCategoria(Model model)
+	{
+		model.addAttribute("lista_categorias", this.categoriaService.obtenerTodos());
+		return "categoria/lista_categoria";
+	}
 	
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public String crearCategoria(@ModelAttribute Categoria categoria)
